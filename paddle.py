370c4405f13f8ebf44239d8e8d679b736607e96d
Axel <axosar@gmail.com>
2021-01-25 14:39:45 -0600

    Paddle class and movement of paddles

--- paddle.py
+++ paddle.py
@@ -4,12 +4,12 @@ screen = Screen()
 
 class Paddle(Turtle):
 
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.shapesize(5, 1)
         self.penup()
-        self.goto(350, 0)
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 20
