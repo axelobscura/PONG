fbd8819fdffd367f9c72b860c4c075629d5cf7b8
Axel <axosar@gmail.com>
2021-01-25 09:26:30 -0600

    Moving the panel

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("orange")
 screen.title("PING POING GAME IN PYTHON")
+screen.tracer(0)
+screen.listen()
 
 paddle = Paddle()
 
-paddle
+screen.onkey(paddle.go_up, "Up")
+screen.onkey(paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,5 +1,4 @@
-from turtle import Screen
-from turtle import Turtle
+from turtle import Screen, Turtle
 
 screen = Screen()
 
@@ -11,10 +10,6 @@ class Paddle(Turtle):
         self.shapesize(5, 1)
         self.penup()
         self.goto(350, 0)
-
-        screen.listen()
-        screen.onkey(self.go_up(), "Up")
-        screen.onkey(self.go_down(), "Down")
 
     def go_up(self):
         new_y = self.ycor() + 20
